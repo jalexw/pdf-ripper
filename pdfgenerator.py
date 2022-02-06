8cc1931e08aa78092ccfb2fd6abf753c3ed9fe77
= <=>
2022-02-05 21:58:37 -0500

    Allow making a PDF from just a single screenshot

--- pdfgenerator.py
+++ pdfgenerator.py
@@ -1,11 +1,22 @@
+import os
+
+outputDir = "output/"
+
 # This function takes a list of images and combines them into a single PDF file.
 def combine_images_into_pdf(images: list, output_file_name: str):
-  if (len(images) <= 1):
-    raise Exception("Must have at least two images to combine. Just take a screenshot if you just want one page.")
-  
-  img1 = images[0]
-  imgs = images[1:]
+  # Make sure the output/ directory exists
+  if not os.path.exists(outputDir):
+    os.mkdir(outputDir)
 
-  img1.save(output_file_name, "PDF", resolution=100.0, save_all=True, append_images=imgs)
+  # Ensure there are images to be pade into a PDF
+  if (len(images) == 0):
+    raise Exception("No images passed to function! Error!")
 
-  return
+  # Create a new PDF file
+  outputPath = outputDir + output_file_name
+  if (len(images) == 1): # Only 1 image, don't need to combine
+    images[0].save(outputPath, "PDF", resolution=100.0)
+    return
+  else: # There are multiple images, so append them to the PDF
+    images[0].save(outputPath, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
+    return
